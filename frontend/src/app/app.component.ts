58dc616b4a1c6cdff726ad0b777824ba6a083067
Pedro Moura <pedrohmoura19@gmail.com>
2025-04-23 18:23:29 -0300

    Created Navbar component.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LocationsService } from './shared/services/locations.service';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'navbar',
+  imports: [],
+  templateUrl: './navbar.component.html',
+  standalone: true,
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+
+}
